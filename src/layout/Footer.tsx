2160147a61cac3ab7c9044a863c8292391c9d092
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-06 19:36:33 +0900

    udpate: footer style

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,7 @@ import type { VFC } from "react";
 export const Footer: VFC = () => {
   return (
     <footer className="p-2 text-center">
-      <small>&copy; 2022 OpenFruits</small>
+      <small className="font-light">&copy; 2022 OpenFruits</small>
     </footer>
   );
 };
