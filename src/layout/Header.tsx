0f69810e8011388a88ab888962c5ffbef09256f1
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-07 04:11:54 +0900

    feat: theme changer

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,6 @@
 import type { VFC } from "react";
 import { NavLinks } from "src/component/NavLinks";
+import { ThemeChanger } from "src/component/ThemeChanger";
 
 const NAV_ITEMS = [
   { href: "/", label: "Home" },
@@ -10,12 +11,15 @@ const NAV_ITEMS = [
 export const Header: VFC = () => {
   return (
     <header className="mx-4 border-b">
-      <h1 className="pt-2 text-4xl font-extralight">@ OpenFruits</h1>
-      <nav className="text-gray-500">
+      <div className="flex justify-between pt-4">
+        <h1 className="text-4xl font-extralight">@ OpenFruits</h1>
+        <ThemeChanger />
+      </div>
+      <nav className="text-gray-500 dark:text-gray-400">
         {NAV_ITEMS.map((item) => {
           const { href, label } = item;
           return (
-            <NavLinks key={href} href={href} activeClass="text-black">
+            <NavLinks key={href} href={href} activeClass="text-black dark:text-white">
               <a className="inline-block p-4">{label}</a>
             </NavLinks>
           );
