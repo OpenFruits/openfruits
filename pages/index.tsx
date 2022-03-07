3e2aded8ad2e609fa0af41a536ebfe756ef9da14
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-07 16:03:51 +0900

    feat: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,156 @@
-import type { CustomNextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { Button } from "src/component/Button";
-import { FluidLayout } from "src/layout/FluidLayout";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { GithubIcon } from "src/component/icon/Github";
+import { GithubOutlineIcon } from "src/component/icon/GithubOutline";
+import { LinkIcon } from "src/component/icon/Link";
+import { TwitterIcon } from "src/component/icon/Twitter";
+import { ZennIcon } from "src/component/icon/Zenn";
 
-const Home: CustomNextPage = () => {
-  const handleClick = () => {
-    alert("Hello World!");
-  };
+const Home: NextPage = () => {
+  const [isLoadingLinks, setIsLoadingLinks] = useState(true);
+  const [isLoadingProjects, setIsLoadingProjects] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoadingLinks(false);
+    }, 500);
+    setTimeout(() => {
+      setIsLoadingProjects(false);
+    }, 1000);
+  }, []);
+
+  const LINK_ITEMS = [
+    {
+      label: "Github | OpenFruits",
+      icon: <GithubIcon fill="#181717" size={60} />,
+      href: "https://github.com/OpenFruits",
+    },
+    {
+      label: "Twitter | @OpenFruits1996",
+      icon: <TwitterIcon fill="#1DA1F2" size={60} />,
+      href: "https://twitter.com/OpenFruits1996",
+    },
+    {
+      label: "Zenn | たくみ",
+      icon: <ZennIcon fill="#3EA8FF" size={60} />,
+      href: "https://zenn.dev/taku1115",
+    },
+  ];
+
+  const PROJECT_ITEMS = [
+    {
+      title: "Next Starter",
+      description: "",
+      imageURL: "/next-starter.png",
+      href: "",
+      github: "https://github.com/OpenFruits/next-starter",
+    },
+    {
+      title: "Next Sandbox",
+      description: "",
+      imageURL: "/next-sandbox.png",
+      href: "https://next-sandbox-eight.vercel.app/",
+      github: "https://github.com/OpenFruits/next-sandbox",
+    },
+    {
+      title: "Parable Box",
+      description: "",
+      imageURL: "/dualing.png",
+      href: "",
+      github: "https://github.com/OpenFruits/parable-box",
+    },
+    {
+      title: "Dualing.app",
+      description: "体育会就活マッチングサービス",
+      imageURL: "/dualing.png",
+      href: "https://www.dualing.app/",
+    },
+    {
+      title: "WithAth-hp",
+      description: "株式会社ウィズアスのホームページ",
+      imageURL: "/withath-hp.jpeg",
+      href: "https://with-ath.vercel.app/",
+      github: "https://github.com/OpenFruits/withath-hp",
+    },
+  ];
 
   return (
     <>
       <Head>
-        <title>Index Page</title>
+        <title>OpenFruits | Home</title>
       </Head>
-
-      <div className="space-y-4">
-        <h2>Index</h2>
-        <Button variant="solid-blue" className="p-2 rounded" onClick={handleClick}>
-          show alert!
-        </Button>
-      </div>
+      <h2 className="pb-2 text-xl">Links</h2>
+      {isLoadingLinks ? (
+        <ul className="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-3">
+          {[...Array(LINK_ITEMS.length)].map((_, index) => (
+            <li key={`loading_${index}`} className="max-w-[230px] bg-slate-200 rounded-sm border animate-pulse">
+              <div className="h-[135px]" />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul className="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-3">
+          {LINK_ITEMS.map((item) => (
+            <li
+              key={item.label}
+              className="max-w-[230px] text-gray-700 hover:text-gray-900 hover:bg-slate-50 dark:bg-slate-100 dark:hover:bg-slate-200 rounded-sm border"
+            >
+              <Link href={item.href}>
+                <a target="_blank" rel="noreferrer">
+                  <div className="grid place-items-center h-24">{item.icon}</div>
+                  <p className="p-2 text-sm border-t dark:border-slate-300">{item.label}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <h2 className="py-2 text-xl">Projects</h2>
+      {isLoadingProjects ? (
+        <ul className="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-3">
+          {[...Array(PROJECT_ITEMS.length)].map((_, index) => (
+            <li key={`loading_${index}`} className="max-w-[230px] bg-slate-200 rounded-sm border animate-pulse">
+              <div className="h-[173px]" />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul className="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-3">
+          {PROJECT_ITEMS.map((item) => (
+            <li key={item.title} className="max-w-[230px] text-gray-700 dark:bg-slate-100 rounded-sm border">
+              <div className="grid place-items-center h-24">
+                <Image src={item.imageURL || ""} alt={item.title} width={230} height={96} />
+              </div>
+              <p className="p-2 text-sm border-t dark:border-slate-300">{item.title}</p>
+              <div className="flex gap-4 justify-center mb-2 text-sm">
+                {item.github ? (
+                  <Link href={item.github}>
+                    <a target="_blank" rel="noreferrer">
+                      <button className="flex gap-2 items-center p-1 hover:text-white hover:bg-slate-800 rounded-md border border-gray-500">
+                        <GithubOutlineIcon fill="none" size={20} />
+                        Github
+                      </button>
+                    </a>
+                  </Link>
+                ) : null}
+                <Link href={item.href}>
+                  <a target="_blank" rel="noreferrer">
+                    <button className="flex gap-2 items-center p-1 hover:text-white hover:bg-emerald-500 rounded-md border border-gray-500 hover:border-transparent">
+                      <LinkIcon fill="none" size={20} />
+                      Link
+                    </button>
+                  </a>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
-
-Home.getLayout = FluidLayout;
 
 export default Home;
