f4726067bb08ab2577b33b04ec71551967749d0a
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-07 16:05:05 +0900

    feat: update experience page

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,21 +1,43 @@
-import type { CustomNextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import { FixedLayout } from "src/layout/FixedLayout";
 
-const Experience: CustomNextPage = () => {
+const Experience: NextPage = () => {
+  const textClass = "text-black dark:text-white";
+
   return (
     <>
       <Head>
-        <title>Experience Page</title>
+        <title>OpenFruits | Experience</title>
       </Head>
 
-      <div>
-        <h2>経験</h2>
+      <div className="prose">
+        <h4 className={textClass}>2020.05 - 2020.09</h4>
+        <ul className={textClass}>
+          <li>案件：社内ツールの開発</li>
+          <li>担当業務：</li>
+          <li>使用技術：Java</li>
+        </ul>
+        <h4 className={textClass}>2020-9 - 2021-3</h4>
+        <ul className={textClass}>
+          <li>案件：勤怠管理システムのバージョンアップ</li>
+          <li>
+            担当業務：React導入
+            <ul>
+              <li>８割：jQueryからReactへのリプレイス</li>
+              <li>２割：ScalaでAPI開発</li>
+            </ul>
+          </li>
+          <li>使用技術：React・TypeScript・Scala</li>
+        </ul>
+        <h4 className={textClass}>2021.04 -</h4>
+        <ul className={textClass}>
+          <li>案件：体育会向け就活支援サービスの開発</li>
+          <li>担当業務：企画、要件定義、設計、開発、リリース</li>
+          <li>使用技術：Next.js・TypeScript・Firebase</li>
+        </ul>
       </div>
     </>
   );
 };
-
-Experience.getLayout = FixedLayout;
 
 export default Experience;
