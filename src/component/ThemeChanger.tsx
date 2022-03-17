3384207d2ab55b13a713bea9052739afc4e25e92
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-18 06:44:02 +0900

    feat: add tooltip for theme changer

--- src/component/ThemeChanger.tsx
+++ src/component/ThemeChanger.tsx
@@ -15,8 +15,13 @@ export const ThemeChanger: VFC = () => {
   const handleClick = () => setTheme(oppositeColor);
 
   return (
-    <button className="text-3xl" onClick={handleClick}>
-      {resolvedTheme === "dark" ? <SunLight height={30} /> : <HalfMoon height={30} />}
-    </button>
+    <div className="group relative">
+      <button className="text-3xl" onClick={handleClick}>
+        {resolvedTheme === "dark" ? <SunLight height={30} /> : <HalfMoon height={30} />}
+      </button>
+      <div className="hidden group-hover:flex absolute right-1 p-1 w-[120px] text-xs text-center text-gray-500 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 rounded-lg">
+        {`Activate ${oppositeColor} mode`}
+      </div>
+    </div>
   );
 };
